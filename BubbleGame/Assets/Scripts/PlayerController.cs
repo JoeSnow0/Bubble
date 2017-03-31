3a7c6b4d26e4f1c41971f3bef3b99595b2414d2d
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-03-31 12:28:13 +0200

    added arrow rotation, its broken though. needs fixing. get on it...me.

---
+++ BubbleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Rotation
+    public float rotationSpeed = 100.0F;
+    float rotation;
+    Quaternion playerRotZ;
+    //Limit rotation to 180 degrees or less
+
+    //Shooting
+    float shootSpeed;
+    //shooting should have cooldown
+    //Shooting ball needs a color
+    //
+    
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        //Rotate Player(Arrow)
+        
+        rotation = Input.GetAxisRaw("Horizontal") * -rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, 0, rotation);
+        Debug.Log(GetComponent<Transform>().rotation.z);
+        
+
+        //Shooting
+
+
+    }
+
+}
